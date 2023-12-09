b8e3924f2841a93b5ac2837244d2d2a821e6fea9
Kaan Donmez <kndonmezz@gmail.com>
2023-12-09 07:58:11 +0300

    Color Picker was uploaded

---
+++ Svelte/ColorPicker/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Slider from "./lib/Slider.svelte";
+
+  let redColor = 130;
+  let greenColor = 200;
+  let blueColor = 50;
+</script>
+
+<h1>Color Picker</h1>
+<Slider colorName="Red" bind:colorValue={redColor} />
+<Slider colorName="Green" bind:colorValue={greenColor} />
+<Slider colorName="Blue" bind:colorValue={blueColor} />
+
+<div
+  id="colorShower"
+  style="background-color:rgb({redColor}, {greenColor}, {blueColor} )"
+></div>
+<div>
+  <h3>(RGB: {redColor}, {greenColor}, {blueColor} )</h3>
+</div>
+
+<style>
+  #colorShower {
+    height: 200px;
+    border: 1px dashed black;
+    margin: 0 3px;
+  }
+
+  h3 {
+    text-align: center;
+  }
+</style>
---
+++ Svelte/ColorPicker/src/lib/Slider.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let colorName: string = "Green";
+  export let colorValue: number = 0;
+</script>
+
+<label for={colorName}>{colorName} ({colorValue})</label>
+<input
+  type="range"
+  id={colorName}
+  bind:value={colorValue}
+  min="0"
+  max="255"
+  style:accent-color={colorName}
+/>
+
+<style>
+  input {
+    width: 100%;
+  }
+  label {
+    margin: 3px;
+  }
+</style>
