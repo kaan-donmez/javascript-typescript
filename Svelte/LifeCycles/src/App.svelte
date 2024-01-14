69bed40029a0dfea677ce8b7ad48167d5ed96fe6
Kaan Donmez <kndonmezz@gmail.com>
2024-01-15 00:07:16 +0300

    LifeCycles was uploaded

---
+++ Svelte/LifeCycles/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Counter from "./lib/Counter.svelte";
+  let condition = true;
+</script>
+
+<main>
+  {#if condition}
+    <Counter />
+  {:else}
+    <p>Counter is not rendered</p>
+  {/if}
+
+  <button
+    on:click={() => {
+      condition = !condition;
+    }}
+  >
+    Toggle
+  </button>
+</main>
+
+<style>
+  button {
+    margin: 1rem;
+  }
+
+  p {
+    margin: 0.5rem;
+  }
+</style>
---
+++ Svelte/LifeCycles/src/lib/Counter.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { afterUpdate, beforeUpdate, onDestroy, onMount } from "svelte";
+  let count = 0;
+  onMount(() => {
+    console.log("onMount!");
+  });
+  beforeUpdate(() => {
+    console.log("beforeUpdate!");
+  });
+  afterUpdate(() => {
+    console.log("afterUpdate!");
+  });
+  onDestroy(() => {
+    console.log("onDestroy!");
+  });
+</script>
+
+<main>
+  <button
+    on:click={() => {
+      count++;
+    }}
+  >
+    Counter: {count}
+  </button>
+</main>
+
+<style>
+</style>
