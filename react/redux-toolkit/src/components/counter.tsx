93b3265cc3e308c31aeb8edd5d3120ac2eeb5418
Kaan Donmez <kndonmezz@gmail.com>
2024-02-04 18:44:58 +0300

    Redux toolkit example project was uploaded

---
+++ react/redux-toolkit/src/components/counter.tsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../components/counter-slice";
+import type { RootState } from "../stores/store";
+
+function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
